454425cf47ab0004dd308acfdaac11c0534172fe
Wojtek <78815235+s7awh@users.noreply.github.com>
2025-01-29 20:11:40 +0100

    scrapped old version, everything is build from 0

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-const characters = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
-];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
-console.log(characters.length)
+// let user set the password lenght 
+let passwordLenght = 0;
+const inputLenght = document.querySelector('#pass-lenght');
+inputLenght.addEventListener('change', () => {
+  passwordLenght = inputLenght.value;
+  ;
+})
 
-let password = 0
-
-
-
-
+let passEl1 = document.getElementById("pas1")
+let passEl2 = document.getElementById("pas2")
 
 function generate() {
-    for (let i=0; i<12; i++) {
-        let randomNumber = Math.floor(Math.random()*91)
-        let randomCharacter = characters[randomNumber]
-        password += randomCharacter
-        }
-    
-console.log(password)
+let password1 = 0
+let password2 = 0
+if (passwordLenght >= 8 && passwordLenght <=32) {
+       for (let i=0; i < passwordLenght; i++) {
+        const length = characters.length
+        let randomNum1 = Math.floor(Math.random() * 91)
+        let randomCharacter1 = characters[randomNum1]
+        let randomNum2 = Math.floor(Math.random() * 91)
+        let randomCharacter2 = characters[randomNum2]
+        password1 += randomCharacter1 
+        password2 += randomCharacter2
+        passEl1.innerText = password1
+        passEl2.innerText = password2
+       }           
+   }
+  else {
+    passEl1.innerText = "Wrong lenght of password"
+    passEl2.innerText = "Wrong lenght of password"
+   } 
 }
+
 
